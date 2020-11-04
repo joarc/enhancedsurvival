2b3591134b05331602ab4c7b12e2d26fc793028d
Joar Classon <joarc@joarc.se>
2020-11-04 21:12:17 +0100

    Fixed bugs
    #1 och #2

--- src/main/java/se/joarc/EnhancedSurvival/Blocks/WaypointListener.java
+++ src/main/java/se/joarc/EnhancedSurvival/Blocks/WaypointListener.java
@@ -1,5 +1,6 @@
 package se.joarc.EnhancedSurvival.Blocks;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
@@ -19,9 +20,21 @@ public class WaypointListener implements Listener {
             String activator = sign.getLine(0);
             if (activator.equalsIgnoreCase("[Waypoint]")) {
                 String title = sign.getLine(1);
-                String subtitle = sign.getLine(2);
-                int stay = Integer.parseInt(sign.getLine(3));
-                e.getPlayer().sendTitle(title, subtitle, 20, stay * 20, 20);
+                if (title.equalsIgnoreCase("")) {
+                    return;
+                }
+                String subtitle = "";
+                int stay = 3;
+                sign.getLine(2);
+                if (sign.getLine(2).equalsIgnoreCase("")) {
+                    subtitle = sign.getLine(2);
+                }
+                sign.getLine(3);
+                if (sign.getLine(3).equalsIgnoreCase("")) {
+                    stay = Integer.parseInt(sign.getLine(3));
+
+                }
+                e.getPlayer().sendTitle(ChatColor.translateAlternateColorCodes('&', title), ChatColor.translateAlternateColorCodes('&', subtitle), 20, stay * 20, 20);
             }
         }
     }
--- src/main/java/se/joarc/EnhancedSurvival/Commands/DevCommands.java
+++ src/main/java/se/joarc/EnhancedSurvival/Commands/DevCommands.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import se.joarc.EnhancedSurvival.EnhancedSurvival;
 import se.joarc.EnhancedSurvival.Items.MinecartBoosterListener;
 
 public class DevCommands implements CommandExecutor {
@@ -13,9 +14,13 @@ public class DevCommands implements CommandExecutor {
         if (sender instanceof Player) {
             Player p = (Player)sender;
             if (cmd.getName().equalsIgnoreCase("heal")) {
-                p.setHealth(20);
-                p.setFoodLevel(20);
-                p.setSaturation(20f);
+                if (p.getUniqueId().equals(EnhancedSurvival.JOARC_UUID)) {
+                    p.setHealth(20);
+                    p.setFoodLevel(20);
+                    p.setSaturation(20f);
+                } else {
+                    p.sendMessage("Only Joarc may use this command!");
+                }
             } else if (cmd.getName().equalsIgnoreCase("minecartspeed")) {
                 if (MinecartBoosterListener.speedEnabled) {
                     MinecartBoosterListener.speedEnabled = false;
